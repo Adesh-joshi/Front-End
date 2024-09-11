c3d3eda2018290638a32209afb6942ee00ec59b8
Adseh-joshi <adeshjoshi91@gamil.com>
2024-09-11 20:08:47 +0545

    first coommit

---
+++ SetTimeOut/script.js
@@ -0,0 +1,26 @@
+// setTimeout(`
+//     console.log(true)
+//     console.log("My name is Adesh" )`,2000)
+
+
+// setTimeout(`console.log("Hi-1")`,1000)
+// setTimeout(`console.log("Hi-2")`,2000)    
+// setTimeout(`console.log("Hi-3")`,3000)
+
+
+
+
+console.log('Hi-1');
+
+function hello() {
+    console.log('Hello World!');
+}
+
+for(let i = 1; i <=4; i++){
+    console.log(i);
+}
+
+hello()
+setTimeout(hello, 0)
+
+console.log('Hi-2');
